29292d2e343e34c02045cc4cabc94a4286bbf3f4
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-05 12:49:30 -0400

    Added project Task_0

---
+++ Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу, которая на входе принимает число и выдает его квадрат. 
+Console.WriteLine("Ведите номер  -> ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.WriteLine(numResult);
+Console.ReadKey();
+
