d3a5023a3ee26d86507a2deb17de4139d6eb837d
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-08 10:47:01 -0400

    added program Favor name

---
+++ Exp0005_Favor_Name/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа по вводу имени и выбора любимого имени
+
+ Console.Write("Введите имя пользрвателя ");
+ string username = Console.ReadLine();
+
+ if (username.ToLower() == "маша")
+ {
+     Console.WriteLine("Ура, это же МАША!");
+ }
+ else 
+ {
+     Console.Write("Привет ");
+     Console.WriteLine(username);
+ }
