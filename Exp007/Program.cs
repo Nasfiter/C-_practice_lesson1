4004fa0c87dfe3505512f40ee88cdc54ace366b9
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-08 11:53:01 -0400

    tried to do sicle WHILE

---
+++ Exp007/Program.cs
@@ -0,0 +1,16 @@
+﻿// циклы 
+
+Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 40;
+int ya = 1;
+int xb = 1 , yb = 30;
+int xc = 80, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
